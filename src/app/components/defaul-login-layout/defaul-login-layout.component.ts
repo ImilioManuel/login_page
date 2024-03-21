e019aa9714f604a56763b211d6ab12df1b8796ef
Imílio Manuel <emiliomdmanuel12@gmail.com>
2024-03-21 22:37:15 +0100

    Adicionando novas actualizações

---
+++ src/app/components/defaul-login-layout/defaul-login-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-defaul-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './defaul-login-layout.component.html',
+  styleUrl: './defaul-login-layout.component.scss'
+})
+export class DefaulLoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+}
